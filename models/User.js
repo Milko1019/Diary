aa48c6f01f92bf50c3e88bd983b55134b09c4489
Mike Harvey <mharvey25@gmail.com>
2019-01-30 16:21:36 -0700

    completed models and started on controllers

---
+++ models/User.js
@@ -0,0 +1,43 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+
+    email: {
+        type: String,
+        unique: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    },
+    username: {
+        type: String,
+        trim: true,
+        required: "Username is Required"
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: "Password is Required",
+        validate: [
+            function (input) {
+                return input.length >= 6;
+            },
+            "Password should be longer."
+        ]
+    },
+    entries: [
+        {
+            // Store ObjectIds in the array
+            type: Schema.Types.ObjectId,
+            // The ObjectIds will refer to the ids in the Entry model
+            ref: "Entry"
+        }
+    ]
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var User = mongoose.model("User", UserSchema);
+
+// Export the User model
+module.exports = User;
