852fc94d4e39d5fb3fb40b0fbd70cb0cbb539467
Matt Milkovich <mattmilkovich@MacBook-Pro.local>
2019-01-31 15:10:31 -0600

    backround

---
+++ client/src/pages/Search.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Search(){
+    // state = {
+        
+    // }
+
+    // componentDidMount() {
+       
+    // }       
+
+    // handleInputChange = event => {
+    //     this.setState({ selectedBreed: event.target.value })
+    // }
+
+    // handleFormSubmit = event => {
+    //     event.preventDefault();
+
+    // }
+    
+    // render () {
+        return(
+            <>
+            <br />
+                <div className="card mb-3">
+                <h1>Serach</h1>
+                <form>
+                    <label htmlFor="">Search By Entry Name:</label>
+                    <input />
+                    <button 
+                        type="submit" 
+                        className="btn btn-primary btn-block mt-2" 
+                        // onClick={this.handleFormSubmit}
+                    >   
+                        Search
+                    </button>
+                </form>
+                </div>
+            </>    
+        )
+    // }
+}
+
+export default Search;
